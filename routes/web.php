e2c2796ccda4194fc1957f121298580dc6c4860c
Brian John Gulac <gulac2525@gmail.com>
2025-05-13 02:55:35 +0800

    hello

--- routes/web.php
+++ routes/web.php
@@ -16,33 +16,21 @@ Route::get('/', function () {
     return view('welcome');
 }); 
 
-
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/users', [UserController::class, 'showUsers'])->name('users');
-
-Route::get('/requesting-offices', [RequestingOfficeController::class, 'showRequestingOffices'])->name('requesting-offices')->defaults('type', 'office');
-
-Route::get('/requesting-schools', [RequestingOfficeController::class, 'showRequestingOffices'])->name('requesting-schools')->defaults('type', 'schools');
-
-Route::get('requestors', [RequestorController::class,'showRequestors'])->name('requestors');
-
-Route::get('fund-sources', [FundSourceController::class, 'showFundSources'])->name('fund-sources');
-
-Route::get('receive-requests', [RequestController::class, 'showRequests'])->name('receive-requests');
-
-Route::get('summary-report', [ReportController::class, 'generateMonthlySummary'])->name('summary-report');
-
-Route::get('summary-report-pdf', [PDFController::class, 'generateMonthlySummary'])->name('summary-report-pdf');
-
-Route::get('request-history-report', [ReportController::class, 'requestHistoryReport'])->name('request-history-report');
-
-Route::get('request-history-report-pdf', [PDFController::class, 'requestHistoryReport'])->name('request-history-report-pdf');
-
-Route::get('request-logs-report', [ReportController::class, 'requestLogsReport'])->name('request-logs-report');
-
-Route::get('request-logs-report-pdf', [PDFController::class, 'requestLogsReport'])->name('request-logs-report-pdf');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/users', [UserController::class, 'showUsers'])->name('users');
+    Route::get('/requesting-offices', [RequestingOfficeController::class, 'showRequestingOffices'])->name('requesting-offices')->defaults('type', 'office');
+    Route::get('/requesting-schools', [RequestingOfficeController::class, 'showRequestingOffices'])->name('requesting-schools')->defaults('type', 'schools');
+    Route::get('requestors', [RequestorController::class,'showRequestors'])->name('requestors');
+    Route::get('fund-sources', [FundSourceController::class, 'showFundSources'])->name('fund-sources');
+    Route::get('receive-requests', [RequestController::class, 'showRequests'])->name('receive-requests');
+    Route::get('summary-report', [ReportController::class, 'generateMonthlySummary'])->name('summary-report');
+    Route::get('summary-report-pdf', [PDFController::class, 'generateMonthlySummary'])->name('summary-report-pdf');
+    Route::get('request-history-report', [ReportController::class, 'requestHistoryReport'])->name('request-history-report');
+    Route::get('request-history-report-pdf', [PDFController::class, 'requestHistoryReport'])->name('request-history-report-pdf');
+    Route::get('request-logs-report', [ReportController::class, 'requestLogsReport'])->name('request-logs-report');
+    Route::get('request-logs-report-pdf', [PDFController::class, 'requestLogsReport'])->name('request-logs-report-pdf');
+});
